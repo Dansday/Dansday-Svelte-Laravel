d572a34af494908f2a646c1694f02c1d063c23c0
Dansday <akbaryudhanto49@gmail.com>
2026-08-29 22:23:09 +0800

    Fix slug for c

--- main/src/lib/slug.ts
+++ main/src/lib/slug.ts
@@ -3,6 +3,9 @@ export function makeSlug(name: string): string {
 		.toLowerCase()
 		.normalize('NFKD')
 		.replace(/[̀-ͯ]/g, '')
+		.replace(/#/g, ' sharp ')
+		.replace(/\+/g, ' plus ')
+		.replace(/&/g, ' and ')
 		.replace(/[^a-z0-9\s-]/g, '')
 		.replace(/[\s-]+/g, '-')
 		.replace(/^-+|-+$/g, '');
