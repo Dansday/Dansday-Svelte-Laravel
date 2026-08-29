5e56563e8e770771ef122a41cb2fc1aee8a08767
Dansday <akbaryudhanto49@gmail.com>
2026-08-29 12:49:11 +0800

    Improve test unit

--- admin/tests/Feature/ExampleTest.php
+++ admin/tests/Feature/ExampleTest.php
@@ -6,10 +6,8 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_root_route_sends_a_fresh_install_to_registration(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('/')->assertRedirect('/register');
     }
 }
